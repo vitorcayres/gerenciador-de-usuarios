3123d1814a46d8ce49a488b5180ac546dea9dfc8
Vitor Cayres <vitor.cayres@fs.com.br>
2018-06-19 15:29:16 -0300

    Finalizando Controller de com CRUD de Usuários

--- backend/src/Controllers/UsersController.php
+++ backend/src/Controllers/UsersController.php
@@ -9,73 +9,117 @@ use App\Models\Users;
 
 class UsersController
 {
-
     public function __construct($container){
         $this->container = $container;
     }    
 
     public function list(Request $request, Response $response){
-        $body = Users::listing();
 
-        if (!empty($body)) {
-            return $response->withJson(['status' => 'success', 'data' => $body], 200)
-            ->withHeader('Content-type', 'application/json'); 
-        }else{
-            return $response->withJson(['status' => 'error', 'message' => 'Listagem não encontrada!'], 400)
-            ->withHeader('Content-type', 'application/json');
+        if ($request->isGet()) {
+
+        $res = json_decode(Users::listing());
+
+            switch ($res->status) {
+                case 'success':
+                    return $response->withJson($res, 200)
+                    ->withHeader('Content-type', 'application/json');
+                break;
+
+                default:
+                    return $response->withJson($res, 404)
+                    ->withHeader('Content-type', 'application/json');  
+                break;
+            }
         }
     }
 
     public function listById(Request $request, Response $response, $args){
        
-        if (!empty($args)) {
+        if ($request->isGet()) {
 
-            $id = $args['id'];
-            $body = Users::listingById($id);
+            if(!empty($args['id'])){
 
-            if (!empty($body)) {
-                return $response->withJson(['status' => 'success', 'data' => $body], 200)
-                ->withHeader('Content-type', 'application/json');   
-            }else{
-                return $response->withJson(['status' => 'error', 'message' => 'Usuário não encontrado!'], 400)
-                ->withHeader('Content-type', 'application/json');                
+                $res = json_decode(Users::listById($args['id']));
+
+                switch ($res->status) {
+                    case 'success':
+                        return $response->withJson($res, 200)
+                        ->withHeader('Content-type', 'application/json');
+                    break;
+
+                    default:
+                        return $response->withJson($res, 404)
+                        ->withHeader('Content-type', 'application/json');  
+                    break;
+                }
             }
         }
     }
     
     public function add(Request $request, Response $response){
-        
+
         $params = $request->getParams();
 
-        if(!empty($params)) {
-            
-            $error = [];
-            foreach ($params as $k => $v) {
-                if($v == ''){
-                    $error[] = ['status' => 'error', 'message' => 'O parâmetro '. $k .' é obrigatório'];
+        if ($request->isPost()) {
+
+            if(!empty($params)){
+
+                $validator =  $this->container->validator->validate($request, [
+                    'username' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'password' => [
+                        'rules' => Validation::length(8),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'name' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'usergroup_id' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'superuser' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'workplace_id' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],
+                    'enabled' => [
+                        'rules' => Validation::notBlank(),
+                        'message' => 'Este campo é obrigatório!'
+                    ],                                                                             
+                ]);
+
+                if ($validator->isValid()) {  
+                    $res = json_decode(Users::add($params));    
+
+                    switch ($res->status) {
+                        case 'success':
+                            return $response->withJson($res, 200)
+                            ->withHeader('Content-type', 'application/json');
+                        break;
+
+                        default:
+                            return $response->withJson($res, 404)
+                            ->withHeader('Content-type', 'application/json');  
+                        break;
+                    }
+                } else {
+                    $errors = $validator->getErrors();
+
+                    return $response->withJson($errors, 400)
+                             ->withHeader('Content-type', 'application/json');  
                 }
-            }
 
-            if(count($error) > 0 ){
-                return $response->withJson($error, 401)
-                ->withHeader('Content-type', 'application/json');                      
-            } 
-
-            $res = Users::add($params);
-
-            if(!empty($res)){
- 
-                return $response->withJson(['status' => 'success', 'message' => 'Usuário inserido com sucesso!'], 200)
-                ->withHeader('Content-type', 'application/json');   
- 
             }else{
-                return $response->withJson(['status' => 'error', 'message' => 'Usuário não encontrado!'], 400)
-                ->withHeader('Content-type', 'application/json');                
+                return $response->withJson(['status' => 'error', 'message' => 'Requisição invalida!'], 400)
+                ->withHeader('Content-type', 'application/json');                  
             }
-
-        }else{
-            return $response->withJson(['status' => 'error', 'message' => 'Os parâmetros username e password são obrigatórios!'], 401)
-                ->withHeader('Content-type', 'application/json');
         }
     }
     
@@ -85,7 +129,6 @@ class UsersController
 
             $params = $request->getParams();
             $id = $args['id'];
-            $paramsDB = [];
 
             $validator =  $this->container->validator->validate($request, [
                 'username' => [
@@ -114,43 +157,50 @@ class UsersController
                 ],                                                            
             ]);   
 
-            if ($validator->isValid()) {
-                
-                $paramsDB['username'] = $params['username'];
-                $paramsDB['password'] = $params['password'];
-                $paramsDB['name'] = $params['name'];
-                $paramsDB['usergroup_id'] = $params['usergroup_id'];                               
-                $paramsDB['superuser'] = $params['superuser'];
-                $paramsDB['workplace_id'] = $params['workplace_id'];                
+            if ($validator->isValid()) {  
+                $res = json_decode(Users::edit($id, $params));    
 
-                //$res = Users::edit($id, $paramsDB);
+                switch ($res->status) {
+                    case 'success':
+                        return $response->withJson($res, 200)
+                        ->withHeader('Content-type', 'application/json');
+                    break;
+
+                    default:
+                        return $response->withJson($res, 404)
+                        ->withHeader('Content-type', 'application/json');  
+                    break;
+                }
 
             } else {
                 $errors = $validator->getErrors();
-
                 return $response->withJson($errors, 404)
                          ->withHeader('Content-type', 'application/json');  
             }
         }
-        
-
-
-        // if (!empty($args)) {
-
-        //     if(!empty($request->getParams())){
-    
-        //         $res = Users::edit($args['id'], $request->getParams());
-                
-
-        //     }else{
-        //         return $response->withJson(['status' => 'error', 'message' => 'Corpo da requisição invalido!'], 400)
-        //         ->withHeader('Content-type', 'application/json');                
-        //     }
-        // }
     }
     
-    public function remove(){
-        
+    public function remove(Request $request, Response $response, $args){
+
+        if ($request->isDelete()) {
+            if(!empty($args['id'])){
+
+                $res = json_decode(Users::remove($args['id']));    
+
+                switch ($res->status) {
+                    case 'success':
+                        return $response->withJson($res, 200)
+                        ->withHeader('Content-type', 'application/json');
+                    break;
+
+                    default:
+                        return $response->withJson($res, 404)
+                        ->withHeader('Content-type', 'application/json');  
+                    break;
+                }
+
+            }
+        }
     }
     
 }
--- backend/src/Models/Users.php
+++ backend/src/Models/Users.php
@@ -11,46 +11,109 @@ class Users extends Model{
      * Listagem dos usuarios
      */
     public function listing(){
-        return Users::get();
+
+        $list = Users::get();
+
+        $list = [];
+
+        if(!empty($list)){
+            return json_encode(['status' => 'success', 'data' => $list]);
+        }else{
+            return json_encode(['status' => 'error', 'message' => 'Listagem não encontrada!']);
+        }
+    }
+
+    /**
+     * Listagem dos usuarios por ID
+     */
+    public function listById($id){
+
+        $listById = Users::where('id', $id)->first();
+
+        if(!empty($listById)){
+            return json_encode(['status' => 'success', 'data' => $listById]);
+        }else{
+            return json_encode(['status' => 'error', 'message' => 'Usuário não encontrado!']);
+        }
     }
 
     /**
      * Inserção dos usuarios
      */
-    public function add(array $body){
-        return Users::insertGetId($body);
+    public function add(array $data){
+        $status = false;
+
+        try{
+            $add = Users::insertGetId($data);
+            $status = true;
+        }
+        catch(\Exception $e){
+            return json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+        } 
+
+        if($status){
+            return json_encode(['status' => 'success', 'message' => 'Usuário criado com sucesso!']);
+        }
     }
     
     /**
-     * Listagem dos usuarios por ID
-     */
-    public function listingById($id){
-       return Users::where('id', $id)
-       ->first();
-    }
-
-    /**
      * Editar usuarios por ID
      */
-    public function edit($id, $paramsDB){
+    public function edit($id, array $data){
 
-        $returnDB = Users::find($id);
-        $returnDB['name'] = 'Chora';
-        $returnDB->save();
+        $status = false;
 
+        $update = Users::find($id);
 
-        // return Users::where('id', $id)
-        // ->update($body);
-        #UPDATE `user` SET `name` = 'Vitor dos Santos Cayres' WHERE `user`.`id` = 168;
+        if (!empty($update)) {
+            try{
+                $update = Users::find($id);                       
+                $update->username       = $data['username'];
+                $update->password       = md5($data['password']);
+                $update->name           = $data['name'];
+                $update->usergroup_id   = $data['usergroup_id'];                               
+                $update->superuser      = $data['superuser'];
+                $update->workplace_id   = $data['workplace_id'];
+                $update->save();
+                $status = true;
+            }
+            catch(\Exception $e){
+                return json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+            } 
+
+            if($status){
+                return json_encode(['status' => 'success', 'message' => 'Usuário alterado com sucesso!']);
+            }
+        }else{
+            return json_encode(['status' => 'error', 'message' => 'Usuário não encontrado!']);
+        }
     }
     
     /**
      * Excluir usuarios por ID
      */
     public function remove($id){
-        return Users::where('id', $id)
-        ->delete();
-    }
 
+        $remove = Users::find($id);
+
+        if(!empty($remove)){
+
+            $status = false;
+
+            try {
+                $remove = Users::find($id);
+                $remove->delete();
+                $status = true;
+            } catch(\Exception $e){
+                return json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+            } 
+
+            if($status){
+                return json_encode(['status' => 'success', 'message' => 'Usuário removido com sucesso!']);
+            }
+        }else{
+            return json_encode(['status' => 'error', 'message' => 'Usuário não encontrado!']);
+        }
+    }
 }
 ?>
